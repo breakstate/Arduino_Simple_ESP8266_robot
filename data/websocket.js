ce38737b486f87d35079f3096c463abc25706a25
Brenton Moodley <juganaught72@gmail.com>
2020-07-11 12:44:43 +0200

    Implemented rough keyboard arrow key control

--- data/websocket.js
+++ data/websocket.js
@@ -1,4 +1,44 @@
 
+
+document.addEventListener('keydown', function(event) 
+{ 
+const key = event.key; 
+switch (key) { 
+case "ArrowLeft": 
+document.getElementById("keyOutput").innerHTML = 'Left'; 
+console.log("left");
+setInstruction("Left");
+break; 
+case "ArrowRight": 
+document.getElementById("keyOutput").innerHTML =  'Right';
+setInstruction("Right");
+break; 
+case "ArrowUp": 
+document.getElementById("keyOutput").innerHTML =  'Up'; 
+setInstruction("Forward");
+break; 
+case "ArrowDown": 
+document.getElementById("keyOutput").innerHTML =  'Down'; 
+setInstruction("Backward");
+break; 
+case " ": 
+document.getElementById("keyOutput").innerHTML =  'Space'; 
+setInstruction("Stop");
+break; 
+} 
+}); 
+
+function changeCamSource(id) {
+  var camSource;
+  if (id == "btnWAN") {
+    camSource = "http://102.65.87.193:8080/video";
+  } else {
+    camSource = "http://192.168.1.13:8080/video";
+  }
+  var video = document.getElementById("video");
+  video.src = camSource;
+}
+
 var connection = new WebSocket('ws://' + location.hostname + ':82/', ['arduino']);
 
 connection.onopen = function () {
@@ -36,13 +76,3 @@ function setInstruction(id) {
     connection.send(instruction);
 }
 
-function changeCamSource(id) {
-  var camSource;
-  if (id == "btnWAN") {
-    camSource = "http://102.65.87.193:8080/video"
-  } else {
-    camSource = "http://192.168.1.13:8080/video"
-  }
-  var video = document.getElementById("video");
-  video.src = camSource;
-}
